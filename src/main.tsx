20c10fbd50c21d653eccc803fd573c413107a005
mobaibai <puresilent.com@gmail.com>
2021-06-15 16:17:15 +0800

    antd支持中文

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+// import moment from 'moment';
+import 'moment/dist/locale/zh-cn';
 
+// moment.locale('zh-cn');
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
